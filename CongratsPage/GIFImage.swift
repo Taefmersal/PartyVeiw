5b26792de7e18bd890f40362b02f26a23dcb3f08
Taef mersal <Taefmrrsal@gmail.com>
2024-10-06 10:50:01 +0300

    congarts page

---
+++ CongratsPage/GIFImage.swift
@@ -0,0 +1,8 @@
+//
+//  GIFImage.swift
+//  CongratsPage
+//
+//  Created by Taef Omar Mersal on 28/03/1446 AH.
+//
+
+import Foundation
---
+++ CongratsPage/MandoPage.swift
@@ -0,0 +1,8 @@
+//
+//  MandoPage.swift
+//  CongratsPage
+//
+//  Created by Taef Omar Mersal on 28/03/1446 AH.
+//
+
+import Foundation
